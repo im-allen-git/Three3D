73b44766e9a0b80a77113b045eb7b9e9de9b8fe1
Alan <jxnx888@hotmail.com>
2020-07-17 17:45:21 +0800

    打印机wifi连接页面，密码页面 改html

--- app/src/main/assets/js/printer_intro.js
+++ app/src/main/assets/js/printer_intro.js
@@ -1,12 +1,4 @@
 $(function(){
 
 })
-function changeNav(){
-    if(true){
-        $(".top_menu .back").html('<i class="iconfont">&#xe7d5;</i>')
-    }
-    else{
-        $(".top_menu .back").html('跳过')
-    }
 
-}
