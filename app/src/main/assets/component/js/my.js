c9df241d2284d4daab3e78cc89946e7d55d138f2
prz <3434286109@qq.com>
2020-07-28 11:49:32 +0800

    提交代码

---
+++ app/src/main/assets/component/js/my.js
@@ -0,0 +1,17 @@
+$(function(){
+    // 进入个人中心，获取useId
+    var useId = js.getUserId('userId');
+    alert('进入我的页面')
+     alert('useId ==' + useId);
+    console.log('useId ==' + useId);
+    // 查询用户名
+    /*var userInfoList = js.getUserInfoDataList(useId);
+    alert('用户信息 == ' + Json.stringify(userInfoList);
+    console.log('用户信息 == ' + Json.stringify(userInfoList));*/
+
+    // 修改用户信息
+
+    // 提交修改的昵称
+
+
+})
