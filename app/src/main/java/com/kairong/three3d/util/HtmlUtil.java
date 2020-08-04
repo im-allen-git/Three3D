d5b3f834f5b6ba1f3353110f6247ed2d50f9bd3e
im-allen-git <1071083166@qq.com>
2020-08-04 09:52:56 +0800

    shop远程网络

--- app/src/main/java/com/kairong/three3d/util/HtmlUtil.java
+++ app/src/main/java/com/kairong/three3d/util/HtmlUtil.java
@@ -8,7 +8,7 @@ public class HtmlUtil {
 
     public static final String BULID_MODULE_URL = "file:///android_asset/src/3DPrinting.html";
     public static final String MYMODULE_HTML = "file:///android_asset/src/my_module.html";
-    public static final String SHOP_HTML = "file:///android_asset/src/shopping.html";
+    public static final String SHOP_HTML = "http://192.168.1.67:448/shopping.html";
     public static final String INDEX_HTML = "file:///android_asset/src/index.html";
     public static final String WELCOME_SLIDE_HTML = "file:///android_asset/src/welcome_slide.html";
     public static final String UPLOAD_GCODE_HTML = "file:///android_asset/src/upload_gcode.html";
